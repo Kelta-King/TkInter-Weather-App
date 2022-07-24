7867f152ab724b3fddfe82ff8dd6c027a4ec0351
Kelta-King <kushangshah131@gmail.com>
2022-07-24 23:31:12 +0530

    bg added

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
     window = tk.Tk()
 
     window.title("Kelta Weather App")
+    window.configure(bg='#87CEFA') 
     photo = tk.PhotoImage(file = "Media/icon.png")
+
     window.iconphoto(False, photo)
     window.minsize(width=600, height=400)
 
